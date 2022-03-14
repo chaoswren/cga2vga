aa8ad46156a1564ee4d7ce90ad03e66d99598004
Wren Lynn <wren@azurepufferfish.com>
2022-03-13 23:14:12 -0400

    Misc testing, also experimenting with GPG signing

--- vga_test.py
+++ vga_test.py
@@ -4,9 +4,10 @@ from array import array
 from uctypes import addressof
 
 fclock=125000000 #clock frequency
-vsync_delay=487 #525 lines total, counting porches & blanking intervals
+vsync_delay=488 #525 lines total, counting porches & blanking intervals
 line_width=800 #640x480 has 800 columns
-line_count=524
+line_count=480
+visible_line_width=640
 
 #DMA address constants
 DMA_BASE=0x50000000
@@ -25,8 +26,15 @@ PIO0_BASE      =0x50200000
 PIO0_TXF0      =PIO0_BASE+0x10
 PIO0_SM0_CLKDIV=PIO0_BASE+0xc8
 
+USER_BANK_PAD_CONTROL_BASE = 0x4001c000
+GPIO0_PAD_CONTROL = USER_BANK_PAD_CONTROL_BASE + 0x04
+GPIO1_PAD_CONTROL = USER_BANK_PAD_CONTROL_BASE + 0x08
+GPIO2_PAD_CONTROL = USER_BANK_PAD_CONTROL_BASE + 0x0C
+GPIO3_PAD_CONTROL = USER_BANK_PAD_CONTROL_BASE + 0x10
+GPIO4_PAD_CONTROL = USER_BANK_PAD_CONTROL_BASE + 0x14
+
 #vsync state machine
-#ISR should be set to 38 less than desired line count
+#ISR should be set to 37 less than desired line count
 @asm_pio(sideset_init=PIO.OUT_HIGH)
 def vsync():
     label("pulse")
@@ -39,7 +47,6 @@ def vsync():
     irq(4)
     label("countloop")
     jmp(x_dec, "countloop")      #jump X != 0, post-decrement
-    jmp("pulse")
 
 #hsync state machine
 #y stores total lines
@@ -48,17 +55,43 @@ def vsync():
 def hsync():
     mov(y,isr)
     label("frame_start")
-    wait(1, irq, 4)                 #wait for vsync
+    wait(1, irq, 4)                  #wait for vsync
     label("sync_pulse")
     set(pins,0)                 [5]
-    set(pins,1)                 [31]
-    nop()                       [10]
+    set(pins,1)                 [2]
+    irq(5)                      [31] #set interrupt after back porch
+    nop()                       [5]
+    irq(6)                      [1]
     jmp(x_dec, "sync_pulse")
-    mov(x,y)                        #reset line counter
+    mov(x,y)                         #reset line counter
     jmp("frame_start")
     
+# @asm_pio(out_init=(PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, \
+#                    PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, \
+#                    PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, \
+#                    PIO.OUT_LOW), out_shiftdir=PIO.SHIFT_RIGHT)
+# def pixels():
+#     mov(y,isr)
+#     label("line_start")
+#     wait(1, irq, 5)
+#     label("pixel_start")
+#     pull(noblock)             [1]
+#     out(pins,16)
+#     jmp(x_dec, "pixel_start")     #jump if we're not done with this line
+#     mov(x, osr)                    #we know X is zero now
+#     pull()
+#     out(pins,16)                   #cheep way to write zero to pins during blanking interval
+
+@asm_pio(set_init=(PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW, PIO.OUT_LOW))
+def pixels():
+    wait(1,irq,5)
+    set(pins,31) [31]
+    wait(1,irq,6)
+    set(pins,0)
+
 vsync_sm = StateMachine(0, vsync, freq=31469, sideset_base=Pin(17)) #run at line freq
 hsync_sm = StateMachine(1, hsync, freq=1573438, set_base=Pin(16)) #run at 1/16 pixel clock
+pixels_sm = StateMachine(2, pixels, freq=25175000, set_base=Pin(0)) #run at pixel clock
 
 vsync_sm.active(0)
 vsync_sm.put(vsync_delay)
@@ -71,3 +104,16 @@ hsync_sm.put(line_count)
 hsync_sm.exec("pull()")
 hsync_sm.exec("mov(isr,osr)")
 hsync_sm.active(1)
+
+# pixels_sm.active(0)
+# pixels_sm.put(visible_line_width)
+# pixels_sm.exec("pull()")
+# pixels_sm.exec("mov(y,osr)")
+pixels_sm.active(1)
+
+mem32[GPIO0_PAD_CONTROL] = mem32[GPIO0_PAD_CONTROL] | 0x31 #12ma drive current, fast slew rate
+mem32[GPIO1_PAD_CONTROL] = mem32[GPIO1_PAD_CONTROL] | 0x31
+mem32[GPIO2_PAD_CONTROL] = mem32[GPIO2_PAD_CONTROL] | 0x31
+mem32[GPIO3_PAD_CONTROL] = mem32[GPIO3_PAD_CONTROL] | 0x31
+mem32[GPIO4_PAD_CONTROL] = mem32[GPIO4_PAD_CONTROL] | 0x31
+
